1816882d96ade4ff7420cb7071c206a57bdf075d
Senol Kaçar <kacar.senol@gmail.com>
2025-04-05 14:58:32 +0200

    Initial Commit

---
+++ src/test/java/com/senolkacar/bookinventory/BookInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senolkacar.bookinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
